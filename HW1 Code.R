143672b3cd77bbec40e7025496f0124b597242b9
Jack Schaeffer <jschaeffer@colgate.edu>
2025-01-28 23:29:26 -0500

    Question 1 Completed

---
+++ HW1 Code.R
@@ -0,0 +1,12 @@
+#################################
+#         Question 2            #
+#################################
+#a
+vec=0:10
+
+#b
+solution.vec = 2^(vec+1)+2^(vec-1)
+
+#c
+result.index = which(solution.vec == 40)
+
